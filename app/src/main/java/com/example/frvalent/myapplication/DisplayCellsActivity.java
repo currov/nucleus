39a3c2372c02eb567ee63f4d3c9db3d0823530dd
eugeniasansegundo@gmail.com <eugeniasansegundo@gmail.com>
2017-05-09 00:07:59 +0200

    improvements to LTE settings

--- app/src/main/java/com/example/frvalent/myapplication/DisplayCellsActivity.java
+++ app/src/main/java/com/example/frvalent/myapplication/DisplayCellsActivity.java
@@ -12,13 +12,14 @@ import android.telephony.CellInfo;
 import android.telephony.CellInfoGsm;
 import android.telephony.CellInfoLte;
 import android.telephony.CellInfoWcdma;
+import android.telephony.CellSignalStrengthLte;
 import android.telephony.TelephonyManager;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class DisplayMessageActivity extends AppCompatActivity {
+public class DisplayCellsActivity extends AppCompatActivity {
 
     TextView textView1;
     @Override
@@ -39,11 +40,15 @@ public class DisplayMessageActivity extends AppCompatActivity {
         textView1=(TextView) findViewById(R.id.textView);
         String IMEINumber=tm.getDeviceId();
         String subscriberID=tm.getSubscriberId();
+        String myPhoneNumber = tm.getLine1Number();
+        if (myPhoneNumber.equals("")){
+            myPhoneNumber = "Unavailable";
+        }
         String SIMSerialNumber=tm.getSimSerialNumber();
-        String networkCountryISO=tm.getNetworkCountryIso();
-        String SIMCountryISO=tm.getSimCountryIso();
-        String softwareVersion=tm.getDeviceSoftwareVersion();
-        String voiceMailNumber=tm.getVoiceMailNumber();
+        String networkCountryISO=tm.getNetworkCountryIso().toUpperCase();
+        String SIMCountryISO=tm.getSimCountryIso().toUpperCase();
+        //String softwareVersion=tm.getDeviceSoftwareVersion();
+        //String voiceMailNumber=tm.getVoiceMailNumber();
         String networkOperator=tm.getNetworkOperator();
         String networkOperatorName=tm.getNetworkOperatorName();
 
@@ -58,8 +63,6 @@ public class DisplayMessageActivity extends AppCompatActivity {
 
 
         }
-
-
 
         String strphoneType="";
 
@@ -81,47 +84,52 @@ public class DisplayMessageActivity extends AppCompatActivity {
         //getting information if phone is in roaming
         boolean isRoaming=tm.isNetworkRoaming();
 
-        String info="Phone Details:\n";
-        info+="\n IMEI Number:"+IMEINumber;
-        info+="\n SubscriberID:"+subscriberID;
-        info+="\n Sim Serial Number:"+SIMSerialNumber;
-        info+="\n Network Country ISO:"+networkCountryISO;
-        info+="\n SIM Country ISO:"+SIMCountryISO;
-        info+="\n Software Version:"+softwareVersion;
-        info+="\n Voice Mail Number:"+voiceMailNumber;
-        info+="\n Phone Network Type:"+strphoneType;
-        info+="\n In Roaming? :"+isRoaming;
-        info+="\n Operator number :"+networkOperator;
-        info+="\n Operator name :"+networkOperatorName;
-
+        String info="Phone Number: " + myPhoneNumber + "\n";
+        info+="\n IMEI Number: "+IMEINumber;
+        info+="\n SubscriberID: "+subscriberID;
+        info+="\n Sim Serial Number: "+SIMSerialNumber;
+        info+="\n Network Country: "+networkCountryISO;
+        info+=" SIM Country: "+SIMCountryISO;
+        //info+="\n Software Version: "+softwareVersion;
+        //info+="\n Voice Mail Number: "+voiceMailNumber;
+        //info+="\n Phone Network Type: "+strphoneType;
+        info+="\n In Roaming?: "+isRoaming;
+        info+="\n MCC: "+networkOperator.substring(0,3) + " MNC: "+ networkOperator.substring(4,5) + " Name: "+ networkOperatorName;
 
         //BEGIN OF CELL INFO
 
         List<Location> rslt = new ArrayList<Location>();
+        int i =1;
         for (android.telephony.CellInfo inputCellInfo : allCells) {
             Location cellLocation = null;
             if (inputCellInfo instanceof CellInfoGsm) {
                 CellInfoGsm gsm = (CellInfoGsm) inputCellInfo;
                 CellIdentityGsm id = gsm.getCellIdentity();
                 //cellLocation = db.query(id.getMcc(), id.getMnc(), id.getCid(), id.getLac());
-                info+="\n GSM CELL :";
-                info+="\n GSM CELL LOCATION :"+id.getMcc()+" "+id.getMnc()+" "+id.getCid()+" "+id.getLac();
+                info+="\n GSM CELL"+ i ;
+                info+="\n        MCC:" +id.getMcc()+"   MNC: "+id.getMnc()+"  CID"+id.getCid()+"  LAC"+id.getLac();
             } else if (inputCellInfo instanceof CellInfoWcdma) {
 
                 CellInfoWcdma wcdma = (CellInfoWcdma) inputCellInfo;
                 CellIdentityWcdma id = wcdma.getCellIdentity();
                 //cellLocation = db.query(id.getMcc(), id.getMnc(), id.getCid(), id.getLac());
-                info+="\n WDCMA CELL :";
-                info+="\n WDCMA CELL LOCATION :"+id.getMcc()+" "+id.getMnc()+" "+id.getCid()+" "+id.getLac();
+                info+="\n WCDMA CELL "+ i ;
+                info+="\n        MCC:" +id.getMcc()+"   MNC: "+id.getMnc()+"  CID"+id.getCid()+"  LAC"+id.getLac();
 
             }
             else if (inputCellInfo instanceof CellInfoLte) {
 
                 CellInfoLte lte = (CellInfoLte) inputCellInfo;
+                boolean registered = lte.isRegistered();
                 CellIdentityLte id = lte.getCellIdentity();
+                CellSignalStrengthLte sig = lte.getCellSignalStrength();
                 //cellLocation = db.query(id.getMcc(), id.getMnc(), id.getCid(), id.getLac());
-                info += "\n LTE CELL :" ;
-                info += "\n LTE CELL LOCATION :" + id.getMcc() + " " + id.getMnc() + " " + id.getCi() + " " + id.getTac();
+                //info += "\n LTE CELL  :" + id.getMcc() + " " + id.getMnc() + " " + id.getCi() + " " + id.getTac();
+                info+="\n LTE CELL "+ i ;
+                info+="\n IS REGISTERED" + registered;
+                info+="\n LTE DUMP "+ id ;
+                info+="\n        MCC:" +id.getMcc()+"   MNC: "+id.getMnc()+"  CI: "+id.getCi()+"  TAC: "+id.getTac()  + "PCI " + id.getPci() ;
+                info+="\n        " + -sig.getDbm()/10+" dBm " + sig.getAsuLevel() + "asu (Level "+ sig.getLevel()+"/4)";
 
             }
 
@@ -130,6 +138,7 @@ public class DisplayMessageActivity extends AppCompatActivity {
                 rslt.add(cellLocation);
             }
             System.out.println("No cell type identified");
+            i++;
         }
 
 
--- app/src/main/java/com/example/frvalent/myapplication/MainActivity.java
+++ app/src/main/java/com/example/frvalent/myapplication/MainActivity.java
@@ -1,28 +1,14 @@
 package com.example.frvalent.myapplication;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Location;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telephony.CellIdentityGsm;
-import android.telephony.CellIdentityLte;
-import android.telephony.CellIdentityWcdma;
-import android.telephony.CellInfoGsm;
-import android.telephony.CellInfoLte;
-import android.telephony.CellInfoWcdma;
-import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
-
-import java.io.Console;
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -31,9 +17,6 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int  MY_PERMISSIONS_REQUEST_READ_PHONE_STATE = 23 ;
     private static final int  MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 24 ;
-
-
-
 
 
     @Override
@@ -49,22 +32,11 @@ public class MainActivity extends AppCompatActivity {
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                     Manifest.permission.READ_PHONE_STATE)) {
 
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
             } else {
-
-                // No explanation needed, we can request the permission.
 
                 ActivityCompat.requestPermissions(this,
                         new String[]{Manifest.permission.READ_PHONE_STATE},
                         MY_PERMISSIONS_REQUEST_READ_PHONE_STATE);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-
 
             }
         }
@@ -74,40 +46,27 @@ public class MainActivity extends AppCompatActivity {
                 Manifest.permission.ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
 
-            // Should we show an explanation?
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                     Manifest.permission.ACCESS_COARSE_LOCATION)) {
 
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
             } else {
-
-                // No explanation needed, we can request the permission.
 
                 ActivityCompat.requestPermissions(this,
                         new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
                         MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
 
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-
-
             }
         }
-
 
 
     }
 
     public void sendMessage(View view){ //from the tutorial
 
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+        Intent intent = new Intent(this, DisplayCellsActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.editText);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
 
     }
